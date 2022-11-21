4aad20554c2598f24ad055350944337c1e24a4f1
Erik Behrends <behrends@users.noreply.github.com>
2022-11-21 17:03:42 +0100

    Abstände mit View als Container-Element definieren

--- components/Quote.js
+++ components/Quote.js
@@ -1,18 +1,22 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Quote({ author, text }) {
   return (
-    <>
+    <View style={styles.container}>
       <Text style={styles.text}>{text}</Text>
       <Text style={styles.author}>&mdash; {author}</Text>
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+  },
   text: {
     fontSize: 38,
     fontStyle: 'italic',
+    marginBottom: 10,
   },
   author: {
     fontSize: 24,
