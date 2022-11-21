8dc4d6463a4d618ecf70cda0a379a95ca83c4286
Erik Behrends <behrends@users.noreply.github.com>
2022-11-21 11:56:24 +0100

    Komponenten mit JavaScript stylen

--- components/Quote.js
+++ components/Quote.js
@@ -3,10 +3,13 @@ import { Text } from 'react-native';
 export default function Quote({ author, text }) {
   return (
     <>
-      <Text style={{ fontSize: 38, fontStyle: 'italic' }}>
-        {text}
-      </Text>
-      <Text style={{ fontSize: 24 }}>&mdash; {author}</Text>
+      <Text style={styles.text}>{text}</Text>
+      <Text style={styles.author}>&mdash; {author}</Text>
     </>
   );
 }
+
+const styles = {
+  text: { fontSize: 38, fontStyle: 'italic' },
+  author: { fontSize: 24 },
+};
