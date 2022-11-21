ed66ef033686bd85754a0354beb3c2c115f8414b
Erik Behrends <behrends@users.noreply.github.com>
2022-11-21 11:30:53 +0100

    Text mit inline-Styles anpassen

--- components/Quote.js
+++ components/Quote.js
@@ -3,8 +3,10 @@ import { Text } from 'react-native';
 export default function Quote({ author, text }) {
   return (
     <>
-      <Text>{text}</Text>
-      <Text>&mdash; {author}</Text>
+      <Text style={{ fontSize: 38, fontStyle: 'italic' }}>
+        {text}
+      </Text>
+      <Text style={{ fontSize: 24 }}>&mdash; {author}</Text>
     </>
   );
 }
