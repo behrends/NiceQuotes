c30f3d5882b30b8a2524c46c8a9d93730abca290
Erik Behrends <behrends@users.noreply.github.com>
2022-11-30 12:55:43 +0100

    Modal für Eingabe eines neuen Zitats verwenden

--- components/NewQuote.js
+++ components/NewQuote.js
@@ -1,15 +1,20 @@
-import { Pressable, Text, View } from 'react-native';
+import { Modal, Pressable, Text, View } from 'react-native';
 
 export default function NewQuote({ visible, onCancel }) {
-  if (!visible) return null;
   return (
-    <View style={{ marginBottom: 50 }}>
-      <Text style={{ borderWidth: 3, padding: 30 }}>
-        Neues Zitat eingeben…
-      </Text>
-      <Pressable onPress={onCancel}>
-        <Text style={{ fontSize: 24, padding: 10 }}>abbrechen</Text>
-      </Pressable>
-    </View>
+    <Modal
+      visible={visible}
+      onRequestClose={onCancel}
+      animationType="slide"
+    >
+      <View style={{ marginBottom: 50 }}>
+        <Text style={{ borderWidth: 3, padding: 30 }}>
+          Neues Zitat eingeben…
+        </Text>
+        <Pressable onPress={onCancel}>
+          <Text style={{ fontSize: 24, padding: 10 }}>abbrechen</Text>
+        </Pressable>
+      </View>
+    </Modal>
   );
 }
